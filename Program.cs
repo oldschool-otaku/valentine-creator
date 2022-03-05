7f8a5ce71c69d6c133962a1b1fed56026a1c92a1
denamo <artstarxd@outlook.com>
2022-03-05 11:28:47 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace blyat
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("From: ");
+            string from = Console.ReadLine();
+            Console.WriteLine("To: ");
+            string to = Console.ReadLine();
+            Valentine.Print(from, to);
+        }
+    }
+}
---
+++ valentine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace blyat
+{
+    public static class Valentine
+    {
+        public static void Print(string @from, string to)
+        {
+            Console.Clear();
+            
+            Console.WriteLine(
+                "\n▄█      ▄  █ ██     ▄▄▄▄▀ ▄███▄       ▀▄    ▄ ████▄   ▄       ██▄   ▄███▄     ▄▄▄▄▄   ▄   \n██     █   █ █ █ ▀▀▀ █    █▀   ▀        █  █  █   █    █      █  █  █▀   ▀   █     ▀▄  █  \n██     ██▀▀█ █▄▄█    █    ██▄▄           ▀█   █   █ █   █     █   █ ██▄▄   ▄  ▀▀▀▀▄ █   █ \n▐█     █   █ █  █   █     █▄   ▄▀        █    ▀████ █   █     █  █  █▄   ▄▀ ▀▄▄▄▄▀  █   █ \n ▐        █     █  ▀      ▀███▀        ▄▀           █▄ ▄█     ███▀  ▀███▀           █▄ ▄█ \n         ▀     █                                     ▀▀▀                             ▀▀▀  \n              ▀                                                                           \n");
+            
+            Console.WriteLine("I hate you, desu~");
+            Console.WriteLine("From: " + @from);
+            Console.WriteLine("To: " + to);
+        }
+    }
+}
